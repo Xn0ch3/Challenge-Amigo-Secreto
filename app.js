cd8dd449737bbee0b31ca07178edcc76626eeb70
Xnoch3 <xaviernochelli@gmail.com>
2025-03-11 13:36:40 -0300

    Se agrega el Array de Amigos

--- app.js
+++ app.js
@@ -1,49 +1,65 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+const nuevoAmigo = document.querySelector('#amigo');
+const listaAmigos = document.querySelector('#listaAmigos');
+const resultado = document.querySelector('#resultado');
+
 function asignarTextoElemento(elemento, texto) {
     const elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
 }
 
 function agregarAmigo() {
-    const amigo = document.querySelector('#amigo');
-    const amigos = document.querySelector('#listaAmigos');
-    const resultado = document.querySelector('#resultado');
     
-    if (amigo.value.trim() === '') {
+    if (nuevoAmigo.value.trim() === '') {
         resultado.textContent = 'No hay nombres de amigos en la lista para sortear.';
         return;
     } else {
         resultado.textContent = '';
     }
     
-    const amigoNuevo = document.createElement('li');
-    amigoNuevo.textContent = amigo.value;
-    amigos.appendChild(amigoNuevo);
-    amigo.value = '';
+    amigos.push(nuevoAmigo.value);
+    listaAmigos.textContent = '';
+    nuevoAmigo.value = '';
+    amigos.forEach(amigo => {
+        const amigoElemento = document.createElement('li');
+        amigoElemento.textContent = amigo;
+        listaAmigos.appendChild(amigoElemento);
+    });
+
+    console.log('Amigos:', amigos);
+
 }
 function sortearAmigo() {
     limpiarResultado();
-    const amigos = document.querySelectorAll('#listaAmigos li');
-    const resultado = document.querySelector('#resultado');
 
     if (amigos.length === 0) {
         resultado.textContent = 'No hay nombres de amigos en la lista para sortear.';
         return;
-    }else {
-        resultado.textContent = '';
     }
 
-    const amigoSeleccionado = amigos[Math.floor(Math.random() * amigos.length)];
-    resultado.textContent = 'El amigo secreto es: ' + amigoSeleccionado.textContent;
+    const amigoSecreto = amigos[Math.floor(Math.random() * amigos.length)];
+    const amigoSecretoElemento = document.createElement('li');
+    amigoSecretoElemento.textContent = amigoSecreto;
+    resultado.appendChild(amigoSecretoElemento);
 
-    limpiarAmigos(); // Limpia la lista después de seleccionar el amigo secreto
+    //amigos = amigos.filter(amigo => amigo !== amigoSecreto);
+
+    amigos.splice(0, amigos.length);
+
+    limpiarAmigos(); // Limpia la lista del HTML al terminar, despues de haber seleccionado el amigo secreto.
+
+    console.log('Amigos restantes:', amigos);
+
 }
 
 function limpiarAmigos() {
-    const amigos = document.querySelectorAll('#listaAmigos li');
-    amigos.forEach(amigo => {
-        amigo.remove();
+    const amigosElementos = document.querySelectorAll('#listaAmigos li');
+    amigosElementos.forEach(amigoElemento => {
+        amigoElemento.remove();
     });
+
 }
 
 function limpiarResultado() {
